f6b49d5cead005530991ec4db673502b3757a69b
Oussama FATNASSI <o.fatnassi@ecoris.com>
2022-09-28 13:28:07 +0200

    Added the competitions list

--- index.php
+++ index.php
@@ -1,3 +1,43 @@
+<?php
+//list of fake competitions
+$competitions = [
+    [
+        'id' => 1,
+        'name' => 'Marathon de Paris',
+        'date' => '2021-01-01',
+        'location' => 'Paris',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+    ],
+    [
+        'id' => 2,
+        'name' => 'Gran trail des Mountains',
+        'date' => '2021-01-02',
+        'location' => 'Lyon',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+    ],
+    [
+        'id' => 3,
+        'name' => 'Championnat de France de course à pied',
+        'date' => '2021-01-03',
+        'location' => 'Marseille',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+    ],
+    [
+        'id' => 4,
+        'name' => 'Marathon de Bordeaux',
+        'date' => '2021-01-04',
+        'location' => 'Bordeaux',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+    ],
+    [
+        'id' => 5,
+        'name' => 'PDGA Eurotour 2021 à Lille', 
+        'date' => '2021-01-05',
+        'location' => 'Lille',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+    ]
+];
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +48,24 @@
     <title>Competitions | Association Des Sports</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">Compétitions</h1>
+            </div>
+        </div>
+        <div class="row">
+            <?php foreach ($competitions as $competition) : ?>
+                <div class="col-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $competition['name'] ?></h5>
+                            <p class="card-text"><?= $competition['description'] ?></p>
+                            <a href="show.php?id=<?= $competition['id'] ?>" class="btn btn-primary">Voir la compétition</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
 </body>
 </html>
